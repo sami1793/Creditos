0fe0606636de4867da12b51c0b4abb5bb172d475
sami1793 <sami_17_94@hotmail.com>
2021-05-24 23:33:48 -0300

    Creé PrestamoManager y lo configure.

---
+++ src/main/java/ar/com/ada/creditos/managers/PrestamoManager.java
@@ -0,0 +1,110 @@
+package ar.com.ada.creditos.managers;
+
+import java.util.List;
+import java.util.logging.Level;
+
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import ar.com.ada.creditos.entities.*;
+
+
+
+public class PrestamoManager {
+    protected SessionFactory sessionFactory;
+
+    public void setup() {
+
+        java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure() // configures settings
+                                                                                                  // from
+                                                                                                  // hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception ex) {
+            StandardServiceRegistryBuilder.destroy(registry);
+            throw ex;
+        }
+
+    }
+
+    public void exit() {
+        sessionFactory.close();
+    }
+
+    public void create(Prestamo prestamo) {
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.save(prestamo);
+
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public Prestamo read(int prestamoId) {
+        Session session = sessionFactory.openSession();
+
+        Prestamo prestamo = session.get(Prestamo.class, prestamoId);
+
+        session.close();
+
+        return prestamo;
+    }
+
+    //NO copio el readybydni
+
+    public void update(Prestamo prestamo) {
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.update(prestamo);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void delete(Prestamo prestamo) {
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.delete(prestamo);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    /**
+     * Este metodo en la vida real no debe existir ya que puede haber miles de
+     * usuarios
+     * 
+     * @return
+     */
+    public List<Prestamo> buscarTodos() {
+
+        Session session = sessionFactory.openSession();
+
+        /// NUNCA HARCODEAR SQLs nativos en la aplicacion.
+        // ESTO es solo para nivel educativo
+        Query query = session.createNativeQuery("SELECT * FROM prestamo", Prestamo.class);
+        //query = session.createQuery("From Obse")
+        List<Prestamo> todos = query.getResultList();
+
+        return todos;
+
+    }
+
+    //BuscarPor tampoco lo hago porque era algo de cliente
+
+    
+}
